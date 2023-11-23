1f175a1015ef324abc4c7c5a5772c03ebc53225a
karl-aleksander <KarlTheFish@users.noreply.github.com>
2023-11-23 15:18:07 +0200

    Starting work on front-end

---
+++ components/Navigation.tsx
@@ -0,0 +1,7 @@
+import {Box} from "@mui/system";
+
+export default async function Navigation(){
+    return (
+        <Box>Navbar placeholder</Box>
+    )
+}
---
+++ components/pages/Browse.tsx
@@ -0,0 +1,9 @@
+import Box from "@mui/material/Box";
+
+export default async function Browse(){
+    return (
+        <Box>
+           <p>Hi</p>
+        </Box>
+    )
+}
