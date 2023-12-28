4a727851826124bd4afb1783802c7a21dada803c
KarlTheFish <KarlTheFish@users.noreply.github.com>
2023-12-28 18:32:16 +0200

    Text field colour fix on login page frontend

--- components/pages/Login.tsx
+++ components/pages/Login.tsx
@@ -8,8 +8,8 @@ export default async function Login(){
         <Stack spacing={2} direction="vertical" className={"bg-white p-3 rounded-md w-1/2"}>
             <Stack spacing={2}>
                 <Typography className={"font-bold"} variant="h5">Welcome back!</Typography>
-                <TextField label="Email" size="small" id="email-input"></TextField>
-                <TextField label="Password" size="small" id="password-input" type="password"></TextField>
+                <TextField label="Email" size="small" id="email-input" color="warning"></TextField>
+                <TextField label="Password" size="small" id="password-input" type="password" color="warning"></TextField>
                 <KarlButton variant="contained" text="Sign in"/>
             </Stack>
             <Stack spacing={1} justifyContent={"flex-end"} className={"pl-4"}>
