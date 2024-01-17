2deb4235664807f236a8bdd71f8833347cfa0a7b
karl-aleksander <KarlTheFish@users.noreply.github.com>
2024-01-17 07:42:00 +0200

    Feat: Added recipe rating for logged in users

--- app/Recipe/[id]/page.tsx
+++ app/Recipe/[id]/page.tsx
@@ -7,20 +7,41 @@ import {createClient} from "@/utils/supabase/server";
 import {KarlButton} from "@/components/NewComponents";
 
 export default async function RecipePage({ params: { id } }: { params: { id: string } }) {
+    "use server"
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
 
-    const { data: recipe } = await supabase.from('recipe').select('name, cumulative_rating, imageurl, recipetext, creator_name').match({ id }).single()
+    const { data: recipe } = await supabase.from('recipe').select('id, name, cumulative_rating, imageurl, recipetext, creator_name').match({ id }).single()
+    const {data: {user}, error} = await supabase.auth.getUser()
+    const {data: { session },} = await supabase.auth.getSession();
 
-    const AddRating = async(formData: FormData) => {
+
+    const AddRating = async (formData: FormData) => {
         "use server"
-        const { data: { user }, error} = await supabase.auth.getUser();
-        console.log(error)
-        if(user != null){
+        const cookieStore = cookies();
+        const supabase = createClient(cookieStore);
+        const {data: {user}, error} = await supabase.auth.getUser()
+        const id = formData.get('recipeID')
+        const {data, count} = await supabase.from('recipe_ratings').select('*', {head: false, count: 'exact'}).eq('rating_user_email', user?.email).eq('recipe_id', id).single()
+
+        const rating = formData.get('rating')
+        console.log("id is ", id)
+        if(user){
+            console.log("User present")
+            if(count != 1){
+                const{data, error} = await supabase.from('recipe_ratings').insert({rating_user_email: user.email, recipe_id: id, rating: rating})
+                if(error){
+                    console.log(error)
+                }
+            }
+            else{
+                const{data, error} = await supabase.from('recipe_ratings').update({rating: rating}).eq('rating_user_email', user?.email).eq('recipe_id', id)
+            }
         }
         else{
-            return redirect("/Login")
+            console.log("User not present")
         }
+        return redirect('/')
     }
 
 
@@ -39,10 +60,27 @@ export default async function RecipePage({ params: { id } }: { params: { id: str
                 </Typography>
                 <Divider/>
                 <Typography>Rating: {recipe.cumulative_rating}/5</Typography>
-                <form action={AddRating}>
+                <form className={"space-x-2"} action={AddRating}>
+                    <label>
+                        1 <input type="radio" name="rating" value="1"/>
+                    </label>
+                    <label>
+                        2 <input type="radio" name="rating" value="2"/>
+                    </label>
+                    <label>
+                        3 <input type="radio" name="rating" value="3"/>
+                    </label>
+                    <label>
+                        4 <input type="radio" name="rating" value="4"/>
+                    </label>
+                    <label>
+                        5 <input type="radio" name="rating" value="5"/>
+                    </label>
+                    <input type="hidden" name="recipeID" value={recipe.id} />
+                    <br/>
                     <KarlButton variant="contained" type="submit" text="Rate"/>
                 </form>
                 <Divider/>
             </Stack>
-        </Stack> )
+        </Stack>)
 }
